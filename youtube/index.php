23fdba56e6b896b20fe4defac341b4da6abfe0fb
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2020-12-22 19:47:25 +0700

    new portofolio

---
+++ youtube/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="0;url=pages/index.html">
+<title>Youtube</title>
+<script language="javascript">
+    window.location.href = "https://www.youtube.com/channel/UCvRYwrw3h8O1QZoyyXYv0tA"
+</script>
+</head>
+<body>
+Go to <a href="https://www.youtube.com/channel/UCvRYwrw3h8O1QZoyyXYv0tA">/pages/index.html</a>
+</body>
+</html>
